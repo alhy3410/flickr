d0cf93a0c5efc74d165cdc300c8843d6901d6998
Guest User <student@epicodus.com>
2015-07-09 11:33:43 -0700

    update random picture

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
-    randomImageNumber = rand(1..Photo.all.length)
-    @photo = Photo.find(randomImageNumber)
+    randomImageNumber = rand(0..Photo.all.length - 1)
+    @photo = Photo.all[randomImageNumber]
   end
 
 
